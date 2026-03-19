58081679197ce3f25ef0cb97353255d00b3f7fa2
kikitataki <seizei03010@gmail.com>
2026-03-20 02:36:27 +0900

    sqliteを用いて保存するまでの仮実装

--- app.py
+++ app.py
@@ -1,13 +1,45 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect
 #render_templateでhtmlとpythonを独立させて編集が可能になる
 #htmlはtemplatesの中で編集する
 app = Flask(__name__)
 #__で囲めば自動で変数の中身が変わる
 #name:ファイル名 file:ファイルのフルパス init:初期化
 
+import sqlite3
+
+def connectdb():
+    # atcoder.db というファイルを作成して接続
+    conn = sqlite3.connect('atcoder.db')
+    cursor = conn.cursor()
+    # postsという名前の表を作る（すでにあれば何もしない）
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS posts (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            memo TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+connectdb() # アプリ起動時に実行
+
+
 @app.route('/') #url末尾が/の場所にアクセスするとしたのコードが動く
 def index():
-    return render_template('index.html')
+    # データベースに接続
+    conn = sqlite3.connect('atcoder.db')
+    conn.row_factory = sqlite3.Row  # これを書くと辞書形式（post['title']）で扱える
+    cursor = conn.cursor()
+    
+    # データを取得（新しい順）
+    cursor.execute('SELECT * FROM posts ORDER BY id DESC')
+    posts = cursor.fetchall()  # 全データを取得
+    conn.close()
+
+    # 第二引数で HTML に posts という名前でデータを渡す
+    return render_template('index.html', posts=posts)
+
 
 # 追加画面
 @app.route('/add')
@@ -18,8 +50,15 @@ def add_page():
 def add_post():
     title = request.form.get('title')
     memo = request.form.get('memo')
-    print(f"受け取った記録: {title} / {memo}")
-    return "データを受け取ったよ！"
+
+    # データベースに保存
+    conn = sqlite3.connect('atcoder.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO posts (title, memo) VALUES (?, ?)', (title, memo))
+    conn.commit()
+    conn.close()
+
+    return redirect('/') # 保存が終わったらトップページに戻る
 
 #実行すると__name__は__main__が代入される
 if __name__ == '__main__':
